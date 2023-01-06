556bb3ee0d625bf58e407aa56f2d30aa92bf6566
shubhamshubhankar <shubhamshubhankar.sharma@gmail.com>
2023-01-06 16:31:57 +0530

    Provided appropriate variable names

--- drawBoxes.py
+++ drawBoxes.py
@@ -5,39 +5,39 @@ from skimage import io
 import matplotlib.pyplot as plt
 from skimage.io import imread, imshow
 
-def drawBoxes(acc, refim, im, val, ridx, cidx):
+def drawBoxes(accumulatorArray, referenceImage, mainImage, maximaValue, rowIndex, colIndex):
     # Code for drawing bounding-box in accumulator array...
 
-        acc[ridx - 5:ridx + 5, cidx - 5] = val
-        acc[ridx - 5:ridx + 5, cidx + 5] = val
+        accumulatorArray[rowIndex - 5:rowIndex + 5, colIndex - 5] = maximaValue
+        accumulatorArray[rowIndex - 5:rowIndex + 5, colIndex + 5] = maximaValue
 
-        acc[ridx - 5, cidx - 5:cidx + 5] = val
-        acc[ridx + 5, cidx - 5:cidx + 5] = val
+        accumulatorArray[rowIndex - 5, colIndex - 5:colIndex + 5] = maximaValue
+        accumulatorArray[rowIndex + 5, colIndex - 5:colIndex + 5] = maximaValue
 
         plt.figure(1)
-        imshow(acc)
+        imshow(accumulatorArray)
         plt.show()
 
         # Code for drawing bounding-box in original image at the found location...
 
         # Find the half-width and height of template
-        hheight = np.floor(refim.shape[0] / 2) + 1
-        hwidth = np.floor(refim.shape[1] / 2) + 1
+        halfHeight = np.floor(referenceImage.shape[0] / 2) + 1
+        halfWidth = np.floor(referenceImage.shape[1] / 2) + 1
 
         # Find coordinates of the box
-        rstart = int(max(ridx - hheight, 1))
-        rend = int(min(ridx + hheight, im.shape[0] - 1))
-        cstart = int(max(cidx - hwidth, 1))
-        cend = int(min(cidx + hwidth, im.shape[1] - 1))
+        rowStart = int(max(rowIndex - halfHeight, 1))
+        rowEnd = int(min(rowIndex + halfHeight, mainImage.shape[0] - 1))
+        colStart = int(max(colIndex - halfWidth, 1))
+        colEnd = int(min(colIndex + halfWidth, mainImage.shape[1] - 1))
 
         # Draw the box
-        im[rstart:rend, cstart] = 255
-        im[rstart:rend, cend] = 255
+        mainImage[rowStart:rowEnd, colStart] = 255
+        mainImage[rowStart:rowEnd, colEnd] = 255
 
-        im[rstart, cstart:cend] = 255
-        im[rend, cstart:cend] = 255
+        mainImage[rowStart, colStart:colEnd] = 255
+        mainImage[rowEnd, colStart:colEnd] = 255
 
         # Show the image
-        plt.figure(2), imshow(refim)
-        plt.figure(3), imshow(im)
+        plt.figure(2), imshow(referenceImage)
+        plt.figure(3), imshow(mainImage)
         plt.show()
--- match_table.py
+++ match_table.py
@@ -6,7 +6,7 @@ from skimage import io
 import matplotlib.pyplot as plt
 
 
-def matchTable(im, table):
+def matchTable(mainImage, table):
     """
     :param im: input binary image, for searching template
     :param table: table for template
@@ -16,9 +16,11 @@ def matchTable(im, table):
     # matches the reference table with the given input
     # image for testing generalized Hough Transform
     # m, n = im.shape
-    m = im.shape[0]
-    n = im.shape[1]
-    acc = np.zeros((m+100, n+100))  # accumulator array requires some extra space
+    rows= mainImage.shape[0]
+    cols = mainImage.shape[1]
+
+    # accumulator array requires some extra space
+    accumulatorArray = np.zeros((rows+100, cols+100))  
     
     #print(acc.shape)
 
@@ -31,14 +33,14 @@ def matchTable(im, table):
     # The row runs from 1 to 306.
     # The column runs from 1 to 306.
 
-    for x in range(1, im.shape[0]):
-        for y in range(1, im.shape[1]):
+    for x in range(1, rows):
+        for y in range(1, cols):
             
             # Because it's a binary image, all the points which
             # are not black are boundary points.
             #print("x : ",x , " y: ", y, "\n")
             #print(im[x])
-            if im[x, y] != 0:  # boundary point
+            if mainImage[x, y] != 0:  # boundary point
                 
                 # Finds the gradient from the current point and
                 # stores it into theta.
@@ -48,5 +50,5 @@ def matchTable(im, table):
                 vectors = table[theta]
 
                 for vector in vectors:
-                    acc[vector[0]+x, vector[1]+y] += 1
-    return acc
+                    accumulatorArray[vector[0]+x, vector[1]+y] += 1
+    return accumulatorArray
